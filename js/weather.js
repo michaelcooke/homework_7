98a3265d765e09fad5b8ce88ca1293909f840a75
Michael Cooke <michael.frank.cooke@gmail.com>
2020-12-04 13:40:48 -0500

    Refactor gettingJSON()

--- js/weather.js
+++ js/weather.js
@@ -20,21 +20,14 @@ function gettingJSON () {
   const query = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&units=' + format + '&appid=' + API_KEY
 
   $.getJSON(query, function (json) {
-    // Use returned json to update the values of the three
-    // elements in HTML.
-    // I would print the JSON to the console
-    const city = json.name
-    const icon = json.weather[0].icon
-    const temperature = json.main.temp
-
     document.getElementById('forecast').style.display = 'block'
 
     const tempImg = document.getElementById('tempImg')
     tempImg.alt = json.weather[0].description
-    tempImg.src = 'https://openweathermap.org/img/w/' + icon + '.png'
+    tempImg.src = 'https://openweathermap.org/img/w/' + json.weather[0].icon + '.png'
     tempImg.title = json.weather[0].main
 
-    document.getElementById('loc').innerHTML = city
-    document.getElementById('temp').innerHTML = temperature
+    document.getElementById('loc').innerHTML = json.name
+    document.getElementById('temp').innerHTML = json.main.temp
   })
 }
